595f779c329e3989851fd8ecb451f63b3533ef0b
Atte Tarvainen <tarvainen.atte@gmail.com>
2024-12-30 13:24:47 +0200

    (fix) fix configs

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ mkdir -p "$XDG_CONFIG_HOME"
 mkdir -p "$XDG_CONFIG_HOME"/nixpkgs
 
 # Create symlinks for existing configurations
-ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
+ln -sf "$PWD/config/nvim" "$XDG_CONFIG_HOME"/nvim
 ln -sf "$PWD/config.nix" "$XDG_CONFIG_HOME"/nixpkgs/config.nix
 
 nix-env -iA nixpkgs.myPackages
